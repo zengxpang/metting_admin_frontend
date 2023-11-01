77f586a5cb35f49f6add3ceeb4d90e3b89cfeb7a
zengxpang <zengxpang@163.com>
2023-11-01 09:11:04 +0800

    fix: 完成部分扫码登录功能

---
+++ src/pages/Login/constants.ts
@@ -0,0 +1,7 @@
+export const QRCODE_STATUS_MAP_TITLE = {
+  'no-scan': '未扫描',
+  'scan-wait-confirm': '已扫描，等待用户确认',
+  'scan-confirm': '已扫描，用户同意授权',
+  'scan-cancel': '已扫描，用户取消授权',
+  expired: '已过期',
+};
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,14 +3,48 @@ import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import { Link, request } from '@umijs/max';
 import to from 'await-to-js';
 import { isNull } from 'lodash-es';
-import { message } from 'antd';
+import { Image, message, Tabs } from 'antd';
 import localforage from 'localforage';
 import { useNavigate } from '@umijs/max';
+import { useState } from 'react';
+
+import { QRCODE_STATUS_MAP_TITLE } from './constants';
+
+// no-scan 未扫描
+// scan-wait-confirm -已扫描，等待用户确认
+// scan-confirm 已扫描，用户同意授权
+// scan-cancel 已扫描，用户取消授权
+// expired 已过期
+
+type ILoginType = 'account' | 'scan';
+type IQrcodeStatus =
+  | 'no-scan'
+  | 'scan-wait-confirm'
+  | 'scan-confirm'
+  | 'scan-cancel'
+  | 'expired';
 
 interface ILoginProps {}
 
 const Login = (props: ILoginProps) => {
   const navigate = useNavigate();
+  const [loginType, setLoginType] = useState<ILoginType>('account');
+  const [qrcodeInfo, setQrcodeInfo] = useState<IKeyValue>({});
+  const [qrcodeStatus, setQrcodeStatus] = useState<IQrcodeStatus>('no-scan');
+
+  const generateQrcode = async () => {
+    const [err, res] = await to(request('/qrcode/generate'));
+    if (isNull(err)) {
+      setQrcodeInfo(res);
+    }
+  };
+
+  const checkQrcode = async () => {
+    const [err, res] = await to(request('/qrcode/check'));
+    if (isNull(err)) {
+      console.log(res);
+    }
+  };
 
   const handleFinish = async (values: {
     username: string;
@@ -32,6 +66,14 @@ const Login = (props: ILoginProps) => {
       message.error(err.data || err);
     }
   };
+
+  const handleLoginTypeChange = async (key: string) => {
+    if (key === 'scan') {
+      await generateQrcode();
+    }
+    setLoginType(key as ILoginType);
+  };
+
   return (
     <LoginForm
       title="会议室预定系统管理端"
@@ -42,34 +84,48 @@ const Login = (props: ILoginProps) => {
         password: '123456',
       }}
     >
-      <ProFormText
-        name="username"
-        fieldProps={{
-          size: 'large',
-          prefix: <UserOutlined className={'prefixIcon'} />,
-        }}
-        placeholder={'请输入用户名'}
-        rules={[
-          {
-            required: true,
-            message: '请输入用户名',
-          },
-        ]}
-      />
-      <ProFormText.Password
-        name="password"
-        fieldProps={{
-          size: 'large',
-          prefix: <LockOutlined className={'prefixIcon'} />,
-        }}
-        placeholder={'请输入密码'}
-        rules={[
-          {
-            required: true,
-            message: '请输入密码',
-          },
-        ]}
-      />
+      <Tabs centered activeKey={loginType} onChange={handleLoginTypeChange}>
+        <Tabs.TabPane tab="账户密码登录" key="account" />
+        <Tabs.TabPane tab="扫码登录" key="scan" />
+      </Tabs>
+      {loginType === 'account' && (
+        <>
+          <ProFormText
+            name="username"
+            fieldProps={{
+              size: 'large',
+              prefix: <UserOutlined className={'prefixIcon'} />,
+            }}
+            placeholder={'请输入用户名'}
+            rules={[
+              {
+                required: true,
+                message: '请输入用户名',
+              },
+            ]}
+          />
+          <ProFormText.Password
+            name="password"
+            fieldProps={{
+              size: 'large',
+              prefix: <LockOutlined className={'prefixIcon'} />,
+            }}
+            placeholder={'请输入密码'}
+            rules={[
+              {
+                required: true,
+                message: '请输入密码',
+              },
+            ]}
+          />
+        </>
+      )}
+      {loginType === 'scan' && (
+        <div className={'justify-center items-center flex flex-col'}>
+          <Image src={qrcodeInfo?.image} height={138} width={138} />
+          <h3>{QRCODE_STATUS_MAP_TITLE[qrcodeStatus]}</h3>
+        </div>
+      )}
       <div className="flex justify-between mb-8px ">
         <Link to="/register">注册账号</Link>
         <Link to="/updatePassword">忘记密码</Link>
